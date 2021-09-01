3b6db8de62933305eb3269b74099dcdd88b14e0c
Gabones <gabaoalb@gmail.com>
2021-09-01 00:22:36 -0300

    sqlite

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -10,28 +10,40 @@ class PlacesListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Places'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => Navigator.of(context).pushNamed(AddPlaceScreen.routeName),
-          )
-        ],
-      ),
-      body: Consumer<GreatPlaces>(
-        child: Center(child: Text('Você ainda não possui lugares cadastrados.')),
-        builder: (ctx, greatPlaces, child) =>
-        greatPlaces.items.length <= 0 ?
-        child! : ListView.builder(
-          itemCount: greatPlaces.items.length,
-          itemBuilder: (ctx, i) => ListTile(
-            leading: CircleAvatar(backgroundImage: FileImage(greatPlaces.items[i].image)),
-            title: Text(greatPlaces.items[i].title),
-            onTap: () => null,
-          ),
+        appBar: AppBar(
+          title: Text('Your Places'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () =>
+                  Navigator.of(context).pushNamed(AddPlaceScreen.routeName),
+            )
+          ],
         ),
-      )
-    );
+        body: FutureBuilder(
+          future: Provider.of<GreatPlaces>(context, listen: false)
+              .fetchAndSetPlaces(),
+          builder: (ctx, snapshot) => snapshot.connectionState ==
+                  ConnectionState.waiting
+              ? Center(child: CircularProgressIndicator())
+              : Consumer<GreatPlaces>(
+                  child: Center(
+                      child:
+                          Text('Você ainda não possui lugares cadastrados.')),
+                  builder: (ctx, greatPlaces, child) =>
+                      greatPlaces.items.length <= 0
+                          ? child!
+                          : ListView.builder(
+                              itemCount: greatPlaces.items.length,
+                              itemBuilder: (ctx, i) => ListTile(
+                                leading: CircleAvatar(
+                                    backgroundImage:
+                                        FileImage(greatPlaces.items[i].image)),
+                                title: Text(greatPlaces.items[i].title),
+                                onTap: () => null,
+                              ),
+                            ),
+                ),
+        ));
   }
 }
