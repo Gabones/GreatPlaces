3139af00620747efa4beefc18c4b3688f0cf93cc
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.net>
2021-09-02 13:30:16 -0300

    dynamic map work on web

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+import '../models/place.dart';
+
+class MapScreen extends StatefulWidget {
+  final PlaceLocation initialLocation;
+  final bool isSelecting;
+
+  MapScreen({this.initialLocation = const PlaceLocation(latitude: 51.5,longitude: -0.09), this.isSelecting = false});
+
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dynamic Map'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(widget.initialLocation.latitude, widget.initialLocation.longitude),
+          zoom: 13.0,
+        ),
+        layers: [
+          TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c']
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                point: LatLng(51.5, -0.09),
+                builder: (ctx) =>
+                    Container(
+                      child: FlutterLogo(),
+                    ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 import '../helper/location_helper.dart';
+import '../screens/map_screen.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({Key? key}) : super(key: key);
@@ -16,14 +17,25 @@ class _LocationInputState extends State<LocationInput> {
   Future<void> _getCurrentUserLocation() async {
     final locData = await Location().getLocation();
     final previewUrl = LocationHelper.generatePreview(
-        latitude: locData.latitude!,
-        longitude: locData.longitude!,
+      latitude: locData.latitude!,
+      longitude: locData.longitude!,
     );
     setState(() {
       _previewImageUrl = previewUrl;
     });
     // print(locData.latitude);
     // print(locData.longitude);
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => MapScreen(
+              isSelecting: true,
+            )));
+    if(selectedLocation == null) {
+      return;
+    }
+    // ...
   }
 
   @override
@@ -33,7 +45,8 @@ class _LocationInputState extends State<LocationInput> {
         Container(
           height: 170,
           width: double.infinity,
-          decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
           child: _previewImageUrl == null
               ? Center(
                   child: Text('No Location Chosen'),
@@ -57,7 +70,7 @@ class _LocationInputState extends State<LocationInput> {
             TextButton.icon(
               icon: Icon(Icons.map),
               label: Text('Select on Map'),
-              onPressed: () {},
+              onPressed: _selectOnMap,
             )
           ],
         )
